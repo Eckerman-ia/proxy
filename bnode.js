388d8161615c8309534fa48d6445f5a4bd4c1a1c
_Eckermania <127853724+Eckerman-ia@users.noreply.github.com>
2024-01-30 10:55:31 -0500

    Create bnode.js

---
+++ bnode.js
@@ -0,0 +1,40 @@
+import express from "express";
+import http from "node:http";
+import path from "node:path";
+import createBareServer from "@tomphttp/bare-server-node";
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on("listening", () => {
+  console.log(`Wormhole v1 running at port 6969`);
+});
+
+server.listen({
+  port: 6969,
+});
